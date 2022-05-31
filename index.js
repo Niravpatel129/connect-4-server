76c052d78edea20680890006f2cd4431bf38c1cc
Nirav Patel <niravpatelp129@gmail.com>
2022-05-31 16:46:04 -0400

    fix

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
+var cors = require('cors');
+
+app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: ['http://localhost:3000', '*'],
     methods: ['GET', 'POST'],
   },
 });
